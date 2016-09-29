58ee4ddb578d585bfbf875533dbfd5edb3b5eeb9
wiede5335 <e1425647@student.tuwien.ac.at>
2016-09-29 03:19:06 +0200

    added addLog path

--- server.h
+++ server.h
@@ -6,3 +6,4 @@ static const char *channel = "#custom";
 static const char *host = "irc.server";
 static const char *port = "6667";
 static const char myhostname[] = "tmi.twitch.tv";
+static const char *addLog = "addLog.txt";
